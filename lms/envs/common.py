b73885bd4faacbf935fb7c2c530a2ff5d65e8aa7
David Baumgold <david@davidbaumgold.com>
2015-05-04 16:45:26 -0400

    More UX review changes

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1279,7 +1279,6 @@ PIPELINE_CSS = {
             'sass/application.css',
             'css/edx-cc.css',
             'sass/ie.css',
-            'css/edx-cc-ie7.css',
         ],
         'output_filename': 'css/lms-style-app.css',
     },
